f03e46b132caffa6b1f2f0480ac84a611961c771
cptullio <cptullio@gmail.com>
2017-07-08 18:55:29 -0400

    Subindo versao com package.json

--- node/exec01.js
+++ node/exec01.js
@@ -1,2 +1,18 @@
 const ola = () => console.log('Ola node!')
+const _ = require('lodash')
+const alunos = [{
+  nome: 'Joao',
+  nota: 7.6
+}, {
+  nome: 'Maria',
+  nota: 8.6
+}, {
+  nome: 'Pedro',
+  nota: 8.1
+}]
+const media = _.sumBy(alunos, 'nota') / alunos.length
+console.log(media)
+
+
+
 setInterval(ola, 1000)
